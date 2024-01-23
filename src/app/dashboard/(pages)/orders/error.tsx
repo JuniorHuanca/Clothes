4b027ad93233eac115b98019f83421199354ef4e
JuniorHuanca <JHuanca_21@outlook.com>
2024-01-23 17:09:32 -0500

    routes validation

--- src/app/dashboard/(pages)/orders/error.tsx
+++ src/app/dashboard/(pages)/orders/error.tsx
@@ -1,30 +1,26 @@
-'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
 export default function Error({
   error,
   reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
+    console.error(error);
+  }, [error]);
+
   return (
     <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+      {!error.message.includes("404") && <h2>Something went wrong!</h2>}
+      {error.message.includes("404") && (
+        <h2>Usted no tiene ordenes asignadas aun </h2>
+      )}
+      <button onClick={() => reset()}>Refrescar</button>
     </div>
-  )
+  );
 }
