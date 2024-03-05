91a80b4ef889fcf86db88134e03e55ea2807ca2b
JuniorHuanca <JHuanca_21@outlook.com>
2024-03-05 16:12:34 -0500

    FEATURE: Add all data for page detail

--- src/app/(general)/product/[slug]/page.tsx
+++ src/app/(general)/product/[slug]/page.tsx
@@ -4,7 +4,8 @@ import { useFetch } from "@/hooks/useFetch";
 import { IProduct } from "@/shared/types";
 import { formatPrice } from "@/shared/utils";
 import { notFound } from "next/navigation";
-
+import { Metadata, ResolvingMetadata } from "next";
+import { MinusSquare, PlusSquare } from "lucide-react";
 type Props = {
   params: { slug: string };
 };
@@ -22,16 +23,42 @@ const Detail = async ({ params }: Props) => {
         <div className="col-span-1 lg:col-span-2">
           <Carrosel slides={product.images} />
         </div>
-
-        {/* Detalles */}
         <div className="col-span-1 lg:col-span-2 p-2 md:p-4">
-          <h1 className={`text-xl md:text-2xl antialiased font-bold`}>
-            {product.title}
-          </h1>
-          <p className="text-lg mb-5">{formatPrice(product.price)}</p>
-          {/* <AddToCart product={ product } /> */}
+          <h1 className={`text-xl md:text-2xl font-bold`}>{product.title}</h1>
+          <p className="text-lg mb-8">{formatPrice(product.price)}</p>
           <h3 className="font-bold text-sm">Descripción</h3>
-          <p>{product.description}</p>
+          <p className="mb-4">{product.description}</p>
+          <div>
+            <div className="flex gap-2">
+              {product.sizes.map((size) => (
+                <button
+                  key={size}
+                  type="button"
+                  className="hover:underline text-lg"
+                >
+                  {size}
+                </button>
+              ))}
+            </div>
+            <div className="flex">
+              <button type="button">
+                <MinusSquare size={30} />
+              </button>
+              <span className="w-20 m-3 px-5 py-1 bg-gray-200 text-center rounded">
+                {product.inStock}
+              </span>
+              <button type="button">
+                <PlusSquare size={30} />
+              </button>
+            </div>
+            <p className="text-lg mb-1">Stock: {product.inStock}</p>
+            <button
+              type="button"
+              className="rounded-md bg-rose-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+            >
+              Agregar al carrito
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -39,3 +66,24 @@ const Detail = async ({ params }: Props) => {
 };
 
 export default Detail;
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const product = await useFetch<IProduct>(`/api/v1/products/${params.slug}`);
+
+  return {
+    title: product.title,
+    description: product.description,
+    openGraph: {
+      type: "website",
+      url: "clothes.vercel.app",
+      siteName: "Clothes",
+      title: product.title,
+      description: product.description,
+      images: product.images,
+    },
+  };
+}
